cf8c4e9068565170cb7685d436822261667aa023
abhimanyu <abhimanyu.s.chaudhary@gmail.com>
2018-06-18 01:54:09 +0530

    made landing page better

--- test-electron2/app/src/Components/Pages/LandingPage.js
+++ test-electron2/app/src/Components/Pages/LandingPage.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react';
+import React, { View, Component } from 'react';
 import ReactDOM from 'react-dom';
 // import logo from './logo.svg';
 import Button from '@material-ui/core/Button';
-
+import Icon from '../../Resources/Images/tasksicon.png';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import {
   BrowserRouter as Router,
   Link,
@@ -16,9 +19,23 @@ class LandingPage extends React.Component {
   }
   render () {
   	return (
-      <Link to="/ListsPage">
-         <Button variant="contained" color="primary">List Page</Button>
-      </Link>
+      <div id = "LandingPageMain" style = {{height: '414px'}}>
+      <Grid container wrap="nowrap" spacing={40} alignContent="center" justify ="center" alignItems="center" direction="column">
+        <Grid item>
+                   <img style={{width: '80px', height: '80px'}} src={Icon}/>
+        </Grid>
+        <Grid item>
+            <Typography variant="display1" align="center" component="h3">
+              TASKS
+            </Typography>
+        </Grid>
+        <Grid item>
+            <Link to="/ListsPage">
+               <Button variant="contained" color="white">Start</Button>
+            </Link>
+        </Grid>
+      </Grid>
+      </div>
   		);
 
   }
